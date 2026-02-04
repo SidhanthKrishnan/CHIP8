2b27ce4879b9cf68aeac5ab0446b9c5943684eaf
SidhanthKrishnan <f20231114@pilani.bits-pilani.ac.in>
2026-02-04 15:18:57 +0530

    Add files via upload

---
+++ Source/Chip8.hpp
@@ -0,0 +1,78 @@
+#pragma once
+#include<bits/stdc++.h>
+using namespace std;
+const unsigned int KEY_COUNT = 16;
+const unsigned int MEMORY_SIZE = 4096;
+const unsigned int REGISTER_COUNT = 16;
+const unsigned int STACK_LEVELS = 16;
+const unsigned int VIDEO_HEIGHT = 32;
+const unsigned int VIDEO_WIDTH = 64;
+const unsigned int FONTSET_SIZE = 80;
+const unsigned int FONTSET_START_ADDRESS = 0x50;
+const unsigned int START_ADDRESS = 0x200;
+class Chip8
+{
+public:
+    Chip8();
+    void LoadROM(string filename);
+    void Cycle();
+    uint8_t keypad[KEY_COUNT]{};
+    uint32_t video[VIDEO_WIDTH * VIDEO_HEIGHT]{};
+private:
+    void Table0();
+    void Table8();
+    void TableE();
+    void TableF();
+    void OP_NULL();
+    void OP_00E0();
+    void OP_00EE();
+    void OP_1nnn();
+    void OP_2nnn();
+    void OP_3xkk();
+    void OP_4xkk();
+    void OP_5xy0();
+    void OP_6xkk();
+    void OP_7xkk();
+    void OP_8xy0();
+    void OP_8xy1();
+    void OP_8xy2();
+    void OP_8xy3();
+    void OP_8xy4();
+    void OP_8xy5();
+    void OP_8xy6();
+    void OP_8xy7();
+    void OP_8xyE();
+    void OP_9xy0();
+    void OP_Annn();
+    void OP_Bnnn();
+    void OP_Cxkk();
+    void OP_Dxyn();
+    void OP_Ex9E();
+    void OP_ExA1();
+    void OP_Fx07();
+    void OP_Fx0A();
+    void OP_Fx15();
+    void OP_Fx18();
+    void OP_Fx1E();
+    void OP_Fx29();
+    void OP_Fx33();
+    void OP_Fx55();
+    void OP_Fx65();
+    uint8_t memory[MEMORY_SIZE]{};
+    uint8_t registers[REGISTER_COUNT]{};
+    uint16_t index{};
+    uint16_t pc{};
+    uint8_t delayTimer{};
+    uint8_t soundTimer{};
+    uint16_t stack[STACK_LEVELS]{};
+    uint8_t sp{};
+    uint16_t opcode{};
+    std::default_random_engine randGen;
+    std::uniform_int_distribution<uint8_t> randByte;
+    typedef void(Chip8::*Chip8func)();
+    map<int,Chip8func> table;
+    map<int,Chip8func> table0;
+    map<int,Chip8func> table8;
+    map<int,Chip8func> tableE;
+    map<int,Chip8func> tableF;
+};
